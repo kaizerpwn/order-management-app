0f506a027d636015ca5da9fddf0654344a68d2b4
Ibrahim Okić <ibrahimokic711@gmail.com>
2024-03-17 18:36:31 +0100

    Code refactor for AdminConsoleController

--- app/src/main/java/com/ibrahimokic/ordermanagement/controller/console/AdminConsoleController.java
+++ app/src/main/java/com/ibrahimokic/ordermanagement/controller/console/AdminConsoleController.java
@@ -9,14 +9,10 @@ import com.ibrahimokic.ordermanagement.service.impl.UserServiceImpl;
 import com.ibrahimokic.ordermanagement.utils.Utils;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 public class AdminConsoleController extends ConsoleUserInterface {
     private final User loggedUser;
-
     private final UserRepository userRepository;
-
     private final AddressRepository addressRepository;
 
     public AdminConsoleController(User user, UserRepository userRepository, AddressRepository addressRepository) {
@@ -25,32 +21,24 @@ public class AdminConsoleController extends ConsoleUserInterface {
         this.addressRepository = addressRepository;
     }
 
-    public void adminDashboard(){
+    public void adminDashboard() {
         Utils.clearConsole(20);
         consoleHeader();
+        displayAdminDashboardMenu();
+        int choice = getValidInput(1, 4);
+        processAdminDashboardChoice(choice);
+    }
 
+    private void displayAdminDashboardMenu() {
         System.out.println(String.format("""
                 [OM-APP]: Welcome, %s! Take a moment to choose your next step:
                 1.) Manage users
                 2.) Manage products
                 3.) Manage orders
                 4.) Log out""", loggedUser.getFirstName()));
+    }
 
-        int choice;
-        do {
-            System.out.print(">> Please enter your choice: ");
-
-            while (!scanner.hasNextInt()) {
-                System.out.println("Invalid input. Please use options 1 - 4.");
-                System.out.print("Enter your choice (1 - 4): ");
-                scanner.next();
-            }
-
-            choice = scanner.nextInt();
-            if (choice < 1 || choice > 4)
-                System.out.println("Invalid choice. Please enter 1 - 4.");
-        } while (choice < 1 || choice > 4);
-
+    private void processAdminDashboardChoice(int choice) {
         switch (choice) {
             case 1 -> adminUserManagementOptions();
             case 4 -> {
@@ -63,29 +51,22 @@ public class AdminConsoleController extends ConsoleUserInterface {
     public void adminUserManagementOptions() {
         Utils.clearConsole(20);
         consoleHeader();
+        displayAdminUserManagementMenu();
+        
+        int choice = getValidInput(1, 4);
+        processAdminUserManagementChoice(choice);
+    }
 
+    private void displayAdminUserManagementMenu() {
         System.out.println(String.format("""
                 [OM-APP]: Welcome, %s! Take a moment to choose your next step:
                 1.) List of the users
                 2.) Create new user
                 3.) Delete a user
                 4.) Log out""", loggedUser.getFirstName()));
+    }
 
-        int choice;
-        do {
-            System.out.print(">> Please enter your choice: ");
-
-            while (!scanner.hasNextInt()) {
-                System.out.println("Invalid input. Please use options 1 - 4.");
-                System.out.print("Enter your choice (1 - 4): ");
-                scanner.next();
-            }
-
-            choice = scanner.nextInt();
-            if (choice < 1 || choice > 4)
-                System.out.println("Invalid choice. Please enter 1 - 4.");
-        } while (choice < 1 || choice > 4);
-
+    private void processAdminUserManagementChoice(int choice) {
         switch (choice) {
             case 1 -> adminUserListForm();
             case 2 -> createNewUserForm();
@@ -112,15 +93,10 @@ public class AdminConsoleController extends ConsoleUserInterface {
     public void createNewUserForm() {
         Utils.clearConsole(20);
         consoleHeader();
-
-        System.out.println("OPTIONS: user | admin");
-        System.out.println("OPTIONS: back (return back to the main menu)");
-        System.out.print(">> Please insert which type of account you want to create: ");
-
-        String role = scanner.next();
+        displayUserCreationOptions();
+        String role = scanner.next().toLowerCase();
         scanner.nextLine();
-
-        switch (role.toLowerCase()) {
+        switch (role) {
             case "admin" -> createAdminAccountForm();
             case "user" -> createUserAccountForm();
             case "back" -> adminUserManagementOptions();
@@ -128,15 +104,16 @@ public class AdminConsoleController extends ConsoleUserInterface {
         }
     }
 
+    private void displayUserCreationOptions() {
+        System.out.println("OPTIONS: user | admin");
+        System.out.println("OPTIONS: back (return back to the main menu)");
+        System.out.print(">> Please insert which type of account you want to create: ");
+    }
+
     private void createAdminAccountForm() {
-        System.out.print(">> Please enter the username for the admin: ");
-        String username = scanner.nextLine();
-
-        System.out.print(">> Please enter the password for the admin: ");
-        String password = scanner.nextLine();
-
-        System.out.print(">> Please enter the email address for the admin: ");
-        String email = scanner.nextLine();
+        String username = promptUserInput("username");
+        String password = promptUserInput("password");
+        String email = promptUserInput("email");
 
         User newAdminAccount = User.builder()
                 .username(username)
@@ -145,55 +122,28 @@ public class AdminConsoleController extends ConsoleUserInterface {
                 .email(email)
                 .build();
 
-        UserServiceImpl userService = new UserServiceImpl(userRepository);
-        userService.createUser(newAdminAccount);
-
-        Utils.returnBackToTheMainMenu(scanner);
-        adminUserManagementOptions();
+        saveUserAndReturnToMenu(newAdminAccount);
     }
 
     private void createUserAccountForm() {
-        System.out.print(">> Please enter the username for the user: ");
-        String username = scanner.nextLine();
-
-        System.out.print(">> Please enter the password for the user: ");
-        String password = scanner.nextLine();
-
+        String username = promptUserInput("username");
+        String password = promptUserInput("password");
         String email;
-        boolean userExists;
 
         do {
-            System.out.print(">> Please enter the email address for the user: ");
-            email = scanner.nextLine();
-
+            email = promptUserInput("email");
             if (userRepository.findByEmail(email) != null) {
-                System.out.println("User with this email already exists. Please try different email address.");
-                userExists = true;
-            } else {
-                userExists = false;
+                System.out.println("User with this email already exists. Please try a different email address.");
             }
-        } while (userExists);
+        } while (userRepository.findByEmail(email) != null);
 
-        System.out.print(">> Please enter the first name of the user: ");
-        String name = scanner.nextLine();
-
-        System.out.print(">> Please enter the last name of the user: ");
-        String surname = scanner.nextLine();
-
-        System.out.print(">> Please enter the birth date of the user (yyyy-MM-dd): ");
-        LocalDate birthDate = LocalDate.parse(scanner.nextLine());
-
-        System.out.print(">> Please enter the street address of the user: ");
-        String streetName = scanner.nextLine();
-
-        System.out.print(">> Please enter the postal code of the user: ");
-        String zipCode = scanner.nextLine();
-
-        System.out.print(">> Please enter the city of the user: ");
-        String city = scanner.nextLine();
-
-        System.out.print(">> Please enter the country of the user: ");
-        String country = scanner.nextLine();
+        String name = promptUserInput("first name");
+        String surname = promptUserInput("last name");
+        LocalDate birthDate = LocalDate.parse(promptUserInput("birth date (yyyy-MM-dd)"));
+        String streetName = promptUserInput("street address");
+        String zipCode = promptUserInput("postal code");
+        String city = promptUserInput("city");
+        String country = promptUserInput("country");
 
         Address newAddress = Address.builder()
                 .country(country)
@@ -213,14 +163,10 @@ public class AdminConsoleController extends ConsoleUserInterface {
                 .address(newAddress)
                 .build();
 
-        UserServiceImpl userService = new UserServiceImpl(userRepository);
-        userService.createUser(newUserAccount);
-
-        Utils.returnBackToTheMainMenu(scanner);
-        adminUserManagementOptions();
+        saveUserAndReturnToMenu(newUserAccount);
     }
 
-    public void deleteUserForm() {
+    private void deleteUserForm() {
         Utils.clearConsole(20);
         consoleHeader();
         scanner.nextLine();
@@ -229,18 +175,47 @@ public class AdminConsoleController extends ConsoleUserInterface {
         userConsoleController.showAllUsersTable();
 
         System.out.println(">> Please enter 'ID' of the user you want to delete.");
-        Long userId = scanner.nextLong();
 
+        Long userId = scanner.nextLong();
         UserServiceImpl userService = new UserServiceImpl(userRepository);
 
-        if(userRepository.findById(userId) != null) {
+        if (userRepository.findById(userId).isPresent()) {
             userService.deleteUser(userId);
-            System.out.println("Successfully deleted user with ID: "+ userId);
+            System.out.println("Successfully deleted user with ID: " + userId);
         } else {
             System.out.println("User with that ID does not exist.");
         }
 
         Utils.returnBackToTheMainMenu(scanner);
         adminUserManagementOptions();
+    }
+
+    private String promptUserInput(String fieldName) {
+        System.out.print(String.format(">> Please enter the %s: ", fieldName));
+        return scanner.nextLine();
+    }
+
+    private void saveUserAndReturnToMenu(User user) {
+        UserServiceImpl userService = new UserServiceImpl(userRepository);
+        userService.createUser(user);
+
+        Utils.returnBackToTheMainMenu(scanner);
+        adminUserManagementOptions();
+    }
+
+    private int getValidInput(int min, int max) {
+        int choice;
+        do {
+            System.out.print(">> Please enter your choice: ");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Invalid input. Please use options "+ min +" - "+ max +".");
+                System.out.print("Enter your choice ("+ min +" - "+ max +"): ");
+                scanner.next();
+            }
+            choice = scanner.nextInt();
+            if (choice < min || choice > max)
+                System.out.println("Invalid choice. Please enter "+ min +" - "+ max +".");
+        } while (choice < min || choice > max);
+        return choice;
     }
 }
