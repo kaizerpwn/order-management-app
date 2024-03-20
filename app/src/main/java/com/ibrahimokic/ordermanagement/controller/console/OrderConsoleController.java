8f9f5c1aa0b6895c40c5423372e014bb136cd6f4
Ibrahim Okić <ibrahimokic711@gmail.com>
2024-03-20 14:30:10 +0100

    Export orders in CSV file

--- app/src/main/java/com/ibrahimokic/ordermanagement/controller/console/OrderConsoleController.java
+++ app/src/main/java/com/ibrahimokic/ordermanagement/controller/console/OrderConsoleController.java
@@ -7,6 +7,11 @@ import com.ibrahimokic.ordermanagement.domain.entity.User;
 import com.ibrahimokic.ordermanagement.repository.OrderRepository;
 import com.ibrahimokic.ordermanagement.utils.Utils;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 public class OrderConsoleController extends ConsoleUserInterface {
@@ -22,26 +27,69 @@ public class OrderConsoleController extends ConsoleUserInterface {
     public List<Order> showAllOrdersList() {
         List<Order> orderList = orderRepository.findAll();
 
-        System.out.println("|-------------|---------------------|---------------|---------------------|------------------------------------------|------------------------------------------|");
-        System.out.println("|   Order ID  |         User        |  Order Date   |    Total Amount     |             Delivery Address             |              Source Address              |");
-        System.out.println("|-------------|---------------------|---------------|---------------------|------------------------------------------|------------------------------------------|");
+        System.out.println("|-------------|---------------------|---------------|---------------------|-------------------------------------------------------------|-------------------------------------------------------|");
+        System.out.println("|   Order ID  |        User         |  Order Date   |    Total Amount     |                      Delivery Address                       |                     Source Address                    |");
+        System.out.println("|-------------|---------------------|---------------|---------------------|-------------------------------------------------------------|-------------------------------------------------------|");
 
         for (Order order : orderList) {
             Address deliveryAddress = order.getDeliveryAddress();
             Address sourceAddress = order.getSourceAddress();
 
-            System.out.printf("| %-12s| %s %-12s| %-14s| $%-19s| %-41s| %-41s|%n",
+            System.out.printf("| %-12s| %-20s| %-14s| $%-19s| %-45s| %-45s |%n",
                     order.getOrderId(),
-                    order.getUser().getFirstName(),
-                    order.getUser().getLastName(),
+                    order.getUser().getFirstName() + " " + order.getUser().getLastName(),
                     order.getOrderDate(),
                     order.getTotalAmount(),
                     Utils.formatAddress(deliveryAddress),
                     Utils.formatAddress(sourceAddress));
         }
 
-        System.out.println("|-------------|---------------------|---------------|---------------------|------------------------------------------|------------------------------------------|");
+        System.out.println("|-------------|---------------------|---------------|---------------------|-------------------------------------------------------------|-------------------------------------------------------|");
         return orderList;
+    }
+
+    public void exportOrdersAsExcelFile() {
+        List<Order> orderList = showAllOrdersList();
+
+        System.out.println(">> Enter the file path where you want to export the Excel file:");
+        String filePath = scanner.nextLine();
+
+        try (Workbook workbook = new XSSFWorkbook()) {
+            Sheet sheet = workbook.createSheet("Orders");
+
+            Row headerRow = sheet.createRow(0);
+            String[] headers = {"Order ID", "User ID", "First Name", "Last Name", "Order Date", "Total Amount",
+                    "Delivery Address", "Source Address"};
+            for (int i = 0; i < headers.length; i++) {
+                Cell cell = headerRow.createCell(i);
+                cell.setCellValue(headers[i]);
+            }
+
+            int rowNum = 1;
+            for (Order order : orderList) {
+                Address deliveryAddress = order.getDeliveryAddress();
+                Address sourceAddress = order.getSourceAddress();
+
+                Row row = sheet.createRow(rowNum++);
+
+                row.createCell(0).setCellValue(order.getOrderId());
+                row.createCell(1).setCellValue(order.getUser().getUserId());
+                row.createCell(2).setCellValue(order.getUser().getFirstName());
+                row.createCell(3).setCellValue(order.getUser().getLastName());
+                row.createCell(4).setCellValue(order.getOrderDate().toString());
+                row.createCell(5).setCellValue("$" + order.getTotalAmount().toString());
+                row.createCell(6).setCellValue(Utils.formatAddress(deliveryAddress));
+                row.createCell(7).setCellValue(Utils.formatAddress(sourceAddress));
+            }
+
+            try (FileOutputStream fileOut = new FileOutputStream(filePath)) {
+                workbook.write(fileOut);
+                System.out.println("Excel file has been successfully exported to: " + filePath);
+            }
+
+        } catch (IOException e) {
+            System.out.println("An error occurred while exporting the Excel file: " + e.getMessage());
+        }
     }
 
     public void displayAdminOrderManagementMenu() {
@@ -50,6 +98,7 @@ public class OrderConsoleController extends ConsoleUserInterface {
 
         System.out.println("[OM-APP]: Welcome, " + loggedUser.getFirstName() + "! Take a moment to choose your next step:");
         System.out.println("1.) List of all orders");
-        System.out.println("2.) Return to the main menu");
+        System.out.println("2.) Export orders as CSV file");
+        System.out.println("3.) Return to the main menu");
     }
 }
