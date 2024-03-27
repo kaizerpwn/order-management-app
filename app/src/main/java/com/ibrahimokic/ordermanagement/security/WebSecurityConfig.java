c3e644f0a055030954faa124bdd8534b3b709bc7
Ibrahim Okić <ibrahimokic711@gmail.com>
2024-03-27 12:59:43 +0100

    Role based route protection

--- app/src/main/java/com/ibrahimokic/ordermanagement/security/WebSecurityConfig.java
+++ app/src/main/java/com/ibrahimokic/ordermanagement/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.ibrahimokic.ordermanagement.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -28,6 +29,22 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(registry -> registry
                         .requestMatchers("/api/users/register").permitAll()
                         .requestMatchers("/api/users/login").permitAll()
+
+                        .requestMatchers(HttpMethod.DELETE, "/api/product/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, "/api/product/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/product/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/product").hasRole("ADMIN")
+
+                        .requestMatchers(HttpMethod.DELETE, "/api/address/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, "/api/address/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/address").hasRole("ADMIN")
+
+                        .requestMatchers(HttpMethod.GET, "/api/users").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/users/{id}").hasRole("ADMIN")
+
+                        .requestMatchers(HttpMethod.DELETE, "/api/orders/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/orders").hasRole("ADMIN")
+
                         .anyRequest().authenticated()
                 )
                 .build();
