a9aa922690d0f67aaa36a38af9764c78b431282a
Ibrahim Okić <ibrahimokic711@gmail.com>
2024-05-12 14:20:29 +0200

    Update ProductMapperImpl.java

--- app/src/main/java/com/ibrahimokic/ordermanagement/mapper/impl/ProductMapperImpl.java
+++ app/src/main/java/com/ibrahimokic/ordermanagement/mapper/impl/ProductMapperImpl.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 
 @Component
 public class ProductMapperImpl implements Mapper<Product, ProductDto> {
-    private ModelMapper modelMapper;
+    private final ModelMapper modelMapper;
     public ProductMapperImpl(ModelMapper modelMapper) {
         this.modelMapper = modelMapper;
     }
